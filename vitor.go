95858c266f6e6ae3562dcaa463f25bf28651a446
vitinho2204 <126572273+vitinho2204@users.noreply.github.com>
2023-03-21 10:47:15 -0300

    Add files via upload

---
+++ vitor.go
@@ -0,0 +1,52 @@
+
+package main
+
+import "fmt"
+
+func main() {
+	x := 5
+	y := 10
+
+	if x > y {
+
+		fmt.Println("x é maior que y")
+
+
+		func main() {
+			x := 5
+			y := 10
+
+			if x > y {
+
+				fmt.Println("x é maior que y")
+
+			} else {
+
+				fmt.Println("y é maior que x")
+
+			}
+
+		}
+
+
+		func main() {
+			x := 5
+			y := 10
+
+			if x > y {
+
+				fmt.Println("x é maior que y")
+
+			} else if x < y {
+
+				fmt.Println("y é maior que x")
+
+			} else {
+
+				fmt.Println("x e y são iguais")
+
+			}
+
+		}
+
+
